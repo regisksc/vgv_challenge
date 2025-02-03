a53953358aadec8061ca43764a6901648d569fd3
regisksc <41172158+regisksc@users.noreply.github.com>
2025-02-03 17:16:50 -0300

    feat(domain): adds an unfavorite usecase

---
+++ lib/domain/usecases/unfavorite.dart
@@ -0,0 +1,3 @@
+import 'package:vgv_challenge/domain/domain.dart';
+
+abstract class Unfavorite extends Usecase<Result<void, Failure>, Coffee> {}
--- lib/domain/usecases/usecases.dart
+++ lib/domain/usecases/usecases.dart
@@ -1,4 +1,5 @@
 export 'get_coffee.dart';
 export 'save_coffee.dart';
+export 'unfavorite.dart';
 export 'update_coffee.dart';
 export 'usecase_abstraction.dart';
