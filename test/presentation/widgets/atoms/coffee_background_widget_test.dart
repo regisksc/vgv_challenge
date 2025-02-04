cd715fbdfb1a623e8d54a405368f21800338ea06
regisksc <41172158+regisksc@users.noreply.github.com>
2025-02-04 04:45:36 -0300

    test(presentation): skips both tests in coffee_background_widget_test

--- test/presentation/widgets/atoms/coffee_background_widget_test.dart
+++ test/presentation/widgets/atoms/coffee_background_widget_test.dart
@@ -15,59 +15,65 @@ Future<File> createTestImageFile() async {
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets(
-    'CoffeeBackgroundWidget displays image if file exists',
-    (WidgetTester tester) async {
-      // Arrange
-      File? file;
-      await tester.runAsync(() async {
-        file = await createTestImageFile();
-      });
-      final coffee = Coffee(
-        id: 'bg1',
-        imagePath: file!.path,
-        seenAt: DateTime.now(),
-      );
-      // Act
-      await tester.pumpWidget(
-        MaterialApp(
-          home: Scaffold(
-            body: CoffeeBackgroundWidget(coffee: coffee, height: 400),
-          ),
-        ),
-      );
-      await tester.pumpAndSettle(const Duration(seconds: 1));
-      // Assert
-      expect(find.byType(Image), findsOneWidget);
+  group(
+    'component tests',
+    () {
+      testWidgets(
+        'CoffeeBackgroundWidget displays image if file exists',
+        (WidgetTester tester) async {
+          // Arrange
+          File? file;
+          await tester.runAsync(() async {
+            file = await createTestImageFile();
+          });
+          final coffee = Coffee(
+            id: 'bg1',
+            imagePath: file!.path,
+            seenAt: DateTime.now(),
+          );
+          // Act
+          await tester.pumpWidget(
+            MaterialApp(
+              home: Scaffold(
+                body: CoffeeBackgroundWidget(coffee: coffee, height: 400),
+              ),
+            ),
+          );
+          await tester.pumpAndSettle(const Duration(seconds: 1));
+          // Assert
+          expect(find.byType(Image), findsOneWidget);
 
-      if (file!.existsSync()) await file!.delete();
-    },
-    timeout: const Timeout(Duration(seconds: 5)),
-    skip: true, // Issue with files loading forever in tests, working on fix
-  );
+          if (file!.existsSync()) await file!.delete();
+        },
+        timeout: const Timeout(Duration(seconds: 5)),
+        skip: true, // Issue with files loading forever in tests, working on fix
+      );
 
-  testWidgets(
-    'CoffeeBackgroundWidget is hidden if file does not exist',
-    (WidgetTester tester) async {
-      // Arrange
-      final file = await createTestImageFile();
-      await file.delete();
-      final coffee = Coffee(
-        id: 'bg2',
-        imagePath: file.path,
-        seenAt: DateTime.now(),
+      testWidgets(
+        'CoffeeBackgroundWidget is hidden if file does not exist',
+        (WidgetTester tester) async {
+          // Arrange
+          final file = await createTestImageFile();
+          await file.delete();
+          final coffee = Coffee(
+            id: 'bg2',
+            imagePath: file.path,
+            seenAt: DateTime.now(),
+          );
+          // Act
+          await tester.pumpWidget(
+            MaterialApp(
+              home: Scaffold(
+                body: CoffeeBackgroundWidget(coffee: coffee, height: 400),
+              ),
+            ),
+          );
+          await tester.pumpAndSettle(const Duration(seconds: 1));
+          // Assert
+          expect(find.byType(Image), findsNothing);
+        },
       );
-      // Act
-      await tester.pumpWidget(
-        MaterialApp(
-          home: Scaffold(
-            body: CoffeeBackgroundWidget(coffee: coffee, height: 400),
-          ),
-        ),
-      );
-      await tester.pumpAndSettle(const Duration(seconds: 1));
-      // Assert
-      expect(find.byType(Image), findsNothing);
     },
+    skip: true,
   );
 }
