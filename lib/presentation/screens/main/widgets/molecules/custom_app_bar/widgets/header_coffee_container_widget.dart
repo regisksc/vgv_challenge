e6c285ed5e704d5e115139dd835b7e0c440d1c58
regisksc <41172158+regisksc@users.noreply.github.com>
2025-02-05 02:35:19 -0300

    fix(presentation): fixes clip behavior in header

--- lib/presentation/screens/main/widgets/molecules/custom_app_bar/widgets/header_coffee_container_widget.dart
+++ lib/presentation/screens/main/widgets/molecules/custom_app_bar/widgets/header_coffee_container_widget.dart
@@ -10,49 +10,49 @@ class HeaderCoffeeContainerWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<MainScreenBloc, MainScreenState>(
-      builder: (context, state) {
-        if (state is MainScreenLoading) {
-          return Shimmer.fromColors(
-            baseColor: Colors.brown[300]!,
-            highlightColor: Colors.brown[100]!,
-            period: const Duration(milliseconds: 500),
-            direction: ShimmerDirection.ttb,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.brown[300],
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(16),
-                  topRight: Radius.circular(16),
-                  bottomLeft: Radius.circular(32),
-                  bottomRight: Radius.circular(32),
+    return ClipRRect(
+      borderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(16),
+        topRight: Radius.circular(16),
+        bottomLeft: Radius.circular(32),
+        bottomRight: Radius.circular(32),
+      ),
+      child: BlocBuilder<MainScreenBloc, MainScreenState>(
+        builder: (context, state) {
+          if (state is MainScreenLoading) {
+            return Shimmer.fromColors(
+              baseColor: Colors.brown[300]!,
+              highlightColor: Colors.brown[100]!,
+              period: const Duration(milliseconds: 500),
+              direction: ShimmerDirection.ttb,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.brown[300],
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(16),
+                    topRight: Radius.circular(16),
+                    bottomLeft: Radius.circular(32),
+                    bottomRight: Radius.circular(32),
+                  ),
                 ),
               ),
-            ),
-          );
-        } else if (state is MainScreenLoaded) {
-          final file = state.coffee.asFile;
-          if (file.existsSync()) {
-            return ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(16),
-                topRight: Radius.circular(16),
-                bottomLeft: Radius.circular(32),
-                bottomRight: Radius.circular(32),
-              ),
-              child: Image.file(file, fit: BoxFit.fill),
             );
-          } else {
-            return const Center(
-              child: Text(
-                'Image not available',
-                style: TextStyle(color: Colors.white),
-              ),
-            );
+          } else if (state is MainScreenLoaded) {
+            final file = state.coffee.asFile;
+            if (file.existsSync()) {
+              return Image.file(file, fit: BoxFit.fill);
+            } else {
+              return const Center(
+                child: Text(
+                  'Image not available',
+                  style: TextStyle(color: Colors.white),
+                ),
+              );
+            }
           }
-        }
-        return Container(color: Colors.brown[300]);
-      },
+          return Container(color: Colors.brown[300]);
+        },
+      ),
     );
   }
 }
